6b925d43deae767dba0ff2eb7dab30814b44e264
Ricardo Roldán Fdez <ricky395@hotmail.com>
2017-12-10 17:21:29 +0100

    Añadido slider tamaño tablero

---
+++ Zerogame/Assets/SliderMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    Text text;
+
+    Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void ValueChanged()
+    {
+        text.text = slider.value.ToString();
+
+        if (text.name == "nRows")
+            GameController.Instance.rows = (int)slider.value;
+
+        else if (text.name == "nColumns")
+            GameController.Instance.columns = (int)slider.value;
+    }
+}
